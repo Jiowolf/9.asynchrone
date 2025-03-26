b637bdf4f013f43b75567bc4aa0bdcf8aa9e1ce8
Jiowolf <msel2lachts@gmail.com>
2025-03-26 11:31:06 +0100

    exo 2 ok sans extra

--- 9.2/script.js
+++ 9.2/script.js
@@ -1,2 +1,25 @@
 let INPUT__SELECT = document.querySelector("input");
 let SUBMIT__SELECT = document.querySelector("#submit");
+
+async function selectData(name){
+try {
+    let ageOfName = await fetch(`https://api.agify.io?name=${name}`);
+    let ageOfNameJson = await ageOfName.json();
+    return ageOfNameJson;
+}catch (error) {
+    console.log("404 Not Found");
+    return
+}
+}
+
+SUBMIT__SELECT.addEventListener("click",async function () {
+    let input = INPUT__SELECT.value;
+    let objName = await selectData(input);
+    console.log(objName);
+    if(objName){
+        const div = document.createElement('div');
+        div.innerHTML = `${objName.name} - ${objName.age}`;
+        document.body.appendChild(div)
+    }
+});
+
